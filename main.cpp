b124579a89644000360f6548b9675d42838caedb
csl <3079625093@qq.com>
2022-01-10 13:31:54 +0800

    small change

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "logger.h"
+#include <fstream>
 
 int main(int argc, char const *argv[])
 {
@@ -10,5 +11,14 @@ int main(int argc, char const *argv[])
     ns_log::warning << "this is a 'warning' message. E = " << 2.71f << ns_log::endl;
     ns_log::error << "this is a 'error' message." << ns_log::endl;
     ns_log::fatal << "this is a 'fatal' message." << ns_log::endl;
+
+    std::fstream file("../log.log", std::ios::out);
+    ns_log::setLoggerOS(file);
+    ns_log::info << "this is a 'info' message. Pi = " << 3.14f << ns_log::endl;
+    ns_log::warning << "this is a 'warning' message. E = " << 2.71f << ns_log::endl;
+    ns_log::error << "this is a 'error' message." << ns_log::endl;
+    ns_log::fatal << "this is a 'fatal' message." << ns_log::endl;
+    file.close();
+    
     return 0;
 }
